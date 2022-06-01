081bc46c9082a094df18901db0a6b3152c46305b
shafee1729 <mohammedshafee1729@gmail.com>
2022-06-01 10:30:55 +0530

    First Commit

---
+++ src/test/java/com/flipkart/tests/PlaceOrder.java
@@ -0,0 +1,28 @@
+package com.flipkart.tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class PlaceOrder {
+  @Test(dataProvider = "dp")
+  public void f(Integer n, String s) {
+  }
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+    };
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
